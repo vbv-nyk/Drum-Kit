7329d65d7cd38da407de4641ce71a8ef593ff643
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2022-11-06 20:26:26 +0530

    Added animations

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 "use strict";
 
-console.log("Hello");
+window.addEventListener("keydown", (e) => {
+    const audio = document.querySelector(`audio[data-key = "${e.key}"]`)
+    const key = document.querySelector(`.key[data-key = "${e.key}"]`)
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add(`playing`);
+})
+
+window.addEventListener("keyup", (e) => {
+    const key = document.querySelector(`.key[data-key = "${e.key}"]`);
+    key.classList.remove(`playing`);
+})
+
